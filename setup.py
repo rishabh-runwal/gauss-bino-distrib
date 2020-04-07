8e55b0cd5bfa5088c7844e2f3d63fc5b6729fde4
Rishabh Runwal <rishabhprunwal@gmail.com>
2020-04-07 16:30:04 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss-bino-distrib',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['gauss-bino-distrib'],
+      author = 'Rishabh Runwal',
+      author_email = 'rishabh.runwal@students.vnit.ac.in',
+      zip_safe=False)
